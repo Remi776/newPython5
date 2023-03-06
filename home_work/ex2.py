d7503f782d23761f458e076da889241209c6a814
Remi776 <99408740+Remi776@users.noreply.github.com>
2023-03-06 21:08:19 +0200

    new ex2

--- home_work/ex2.py
+++ home_work/ex2.py
@@ -1,12 +1,8 @@
-num1, num2 = int(input()), int(input())
-
 def summ(a, b):
-    if a == 0 and b == 0:
-        return 0
     if a == 0:
-        return summ(0, b - 1) + 1
-    if b == 0:
-        return summ(a - 1, 0) + 1
-    return summ(a - 1, b - 1) + 2
+        return b
+    return summ(a - 1, b + 1)
 
+
+num1, num2 = int(input()), int(input())
 print(summ(num1, num2))
