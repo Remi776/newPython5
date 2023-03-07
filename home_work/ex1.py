48e5e4aa3f552b1c24045d6b60dbc46eedaa7010
Remi776 <99408740+Remi776@users.noreply.github.com>
2023-03-07 19:04:14 +0200

    ex1 pep8

--- home_work/ex1.py
+++ home_work/ex1.py
@@ -1,8 +1,10 @@
 num1, num2 = int(input()), int(input())
 
+
 def exponentiation(a, b):
     if b == 0:
         return 1
     return exponentiation(a, b - 1) * a
+
 
 print(exponentiation(num1, num2))
